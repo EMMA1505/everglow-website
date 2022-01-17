77827ffd202286ce5b17d65aba99d43a99c27ff9
EMMA1505 <emmanuelroamni@gmail.com>
2022-01-17 16:51:31 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const path = require('path')
+const port = 3000
+const app = express()
+const productsModule = require('./utils/services');
+
+const viewsURL = path.join(__dirname, 'views')
+
+app.set('views', viewsURL)
+app.set('view engine', 'ejs')
+
+const publicURL = path.join(__dirname, 'public')
+
+app.use(express.static(publicURL))
+
+///////////////////////////////////////////////////////////////////
+
+// app.get('', (req, res) => {
+//     res.render('index', {
+//         services
+//     });
+// });
+
+///////////////////////////////////////////////////////////////////
+
+app.get('', (req, res) => {
+    productsModule.getAllServices((err, services) => {
+        if (err) return res.status(500).send(err);
+        console.log(services);
+        return res.render('index', { services });
+    });
+});
+
+// console.log(services)
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// app.get('/agregar', (req, res) => {
+//     res.render('pages/agregar')
+// })
+
+app.listen(port, () => {
+    console.log(`Escuchando el puerto ${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utils/services.js
@@ -0,0 +1,85 @@
+// const services = [
+//     {
+//         id: 1,
+//         service: 'EXPRESS',
+//         image: 'img/express.svg',
+//         alt: 'Limado de uñas',
+//         // tasks: [{task1: ' Limado',}, {task2: ' Pulido',}, {task3: ' Esmaltado'}]
+//         task1: ' Limado',
+//         task2: ' Pulido',
+//         task3: ' Esmaltado'
+        
+//     },
+//     {
+//         id: 2,
+//         service: 'BEAUTY',
+//         image: 'img/beauty.svg',
+//         alt: 'Extracción de cutícula',
+//         task1: ' Limado',
+//         task2: ' Cutícula',
+//         task3: ' Esmaltado'
+//     },
+//     {
+//         id: 3,
+//         service: 'SEMI PERMANENTE',
+//         image: 'img/semi.svg',
+//         alt: 'Uñas pintadas en secado UV',
+//         task1: ' Uñas brillantes y perfectas',
+//         task2: ' Durabilidad entre 15 a 20 días',
+//         task3: ' Variedad de colores'
+//     },
+//     {
+//         id: 4,
+//         service: 'DECO',
+//         image: 'img/deco.svg',
+//         alt: 'Uñas decoradas con dibujos',
+//         task1: ' Diseños exclusivos',
+//         task2: ' Colocación de apliques',
+//         task3: ' Texturas'
+//     },
+//     {
+//         id: 5,
+//         service: 'ESCULPIDAS',
+//         image: 'img/esculpidas.svg',
+//         alt: 'Uñas esculpidas',
+//         task1: ' Formas y extensión',
+//         task2: ' Perfección y naturalidad',
+//         task3: ' Protección de uña'
+//     },
+//     {
+//         id: 6,
+//         service: 'FEET BEAUTY',
+//         image: 'img/pedicure.svg',
+//         alt: 'Esmaltado de uñas de pies',
+//         task1: ' Limado de uñas y durezas',
+//         task2: ' Cutícula',
+//         task3: ' Esmaltado'
+//     },
+// ];
+
+// module.exports = services;
+
+const request = require('postman-request');
+
+const baseURL = 'http://localhost:3001';
+
+const getAllServices = (callback) => {
+  request(`${baseURL}/services`, (err, res) => {
+    if(err) return callback(err, undefined);
+    const allServices = JSON.parse(res.body);
+    return callback(undefined, allServices);
+  });
+};
+
+// const getSingleProduct = (callback) => {
+//   request(`${baseURL}/product/123`, (err, res) => {
+//     if(err) return callback(err, undefined);
+//     const singleProduct = res.body;
+//     return callback(undefined, singleProduct);
+//   });
+// };
+
+
+// module.exports = { getAllProducts, getSingleProduct };
+
+module.exports = { getAllServices }
